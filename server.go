3a034d334f67033bb2621af1eb1c17f0a9ea4caf
Azad Asanali <azneotech@gmail.com>
2022-04-09 12:01:24 +0800

    created the orders app

---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/azad/go-applied-concurrency/handlers"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Welcome to the Orders App!")
+	handler, err := handlers.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	router := handlers.ConfigureHandler(handler)
+	fmt.Println("Listening on localhost:3000...")
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
